338722d17395b2f10b26eacf476849976be11aca
Eliot Alter <eliot.alter@tufts.edu>
2012-03-11 15:16:54 -0400

    Starting to figure out black notes

--- GeometryTunes/GeometryTunes/Piano.m
+++ GeometryTunes/GeometryTunes/Piano.m
@@ -44,9 +44,10 @@
     CGRect screenRect = [self bounds];
     int width = screenRect.size.width;
     int height = screenRect.size.height;
-    const int numNotes = 7; //an octave
-    float octaveButtonRelativeSize = 1.3;
-    float whiteKeyWidth = ((float)width) / (numNotes+octaveButtonRelativeSize*2);
+    const int numWhiteNotes = 7; //an octave
+    const int numNotes = 12;
+    const float octaveButtonRelativeSize = 1.3;
+    const float whiteKeyWidth = ((float)width) / (numWhiteNotes+octaveButtonRelativeSize*2);
     float x = 0;
     
     UIColor *octavesBackground = [UIColor blueColor];
@@ -63,13 +64,29 @@
     [self addSubview:octaveDown];
     x += buttonWidth;
     
-    for(int i=0; i<numNotes; i++, x+= whiteKeyWidth)
+    float blackKeyWidth = whiteKeyWidth/2;
+    float blackKeyHeight = height*2/3;
+    UIButton *note;
+    for(int i=0; i<numNotes; i++)
     {
-        UIButton *note = [[UIButton alloc]initWithFrame:CGRectMake(x, 0, whiteKeyWidth, height)];
-        if(i%2 == 0)
-            [note setBackgroundColor:[UIColor whiteColor]];
+        int relativeNote = i % 12;
+        if(relativeNote == 1 || relativeNote == 4 || relativeNote == 7 || relativeNote == 9 | relativeNote == 11)
+        {
+            NSLog(@"Black note!");
+            //The note is a black note
+            note = [[UIButton alloc]initWithFrame:CGRectMake(x+(whiteKeyWidth*2-blackKeyWidth)/2, height-blackKeyHeight, blackKeyWidth, blackKeyHeight)];
+            [note setBackgroundColor:[UIColor blackColor]];
+        }
         else
-            [note setBackgroundColor:[UIColor greenColor]];
+        {
+            //This note is a white note
+            note = [[UIButton alloc]initWithFrame:CGRectMake(x, 0, whiteKeyWidth, height)];
+            x += whiteKeyWidth;
+            if(i%2 == 0)
+                [note setBackgroundColor:[UIColor whiteColor]];
+            else
+                [note setBackgroundColor:[UIColor greenColor]];
+        }
         [self addSubview:note];
     }
     
