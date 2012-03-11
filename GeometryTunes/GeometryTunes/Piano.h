9690a3fe899b29f52f0b14b2b24023d4e73b2c44
Eliot Alter <eliot.alter@tufts.edu>
2012-03-10 22:21:14 -0500

    Added the piano class

---
+++ GeometryTunes/GeometryTunes/Piano.h
@@ -0,0 +1,13 @@
+//
+//  Piano.h
+//  GeometryTunes
+//
+//  Created by Music2 on 3/10/12.
+//  Copyright (c) 2012 Tufts University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Piano : UIView
+
+@end
---
+++ GeometryTunes/GeometryTunes/Piano.m
@@ -0,0 +1,31 @@
+//
+//  Piano.m
+//  GeometryTunes
+//
+//  Created by Music2 on 3/10/12.
+//  Copyright (c) 2012 Tufts University. All rights reserved.
+//
+
+#import "Piano.h"
+
+@implementation Piano
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
