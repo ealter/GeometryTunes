9b8461ed946a9535461818753023acbdfb2aede6
Eliot Alter <eliot.alter@tufts.edu>
2012-03-20 16:49:02 -0400

    Added the note player class. Also changed the octave ranges. Added an assert to make sure the initial octave was within the correct range

---
+++ GeometryTunes/NotePlayer.h
@@ -0,0 +1,13 @@
+//
+//  NotePlayer.h
+//  GeometryTunes
+//
+//  Created by Music2 on 3/20/12.
+//  Copyright (c) 2012 Tufts University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NotePlayer : NSObject
+
+@end
---
+++ GeometryTunes/NotePlayer.m
@@ -0,0 +1,13 @@
+//
+//  NotePlayer.m
+//  GeometryTunes
+//
+//  Created by Music2 on 3/20/12.
+//  Copyright (c) 2012 Tufts University. All rights reserved.
+//
+
+#import "NotePlayer.h"
+
+@implementation NotePlayer
+
+@end
