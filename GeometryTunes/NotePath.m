1e04cdf46ecc599c65f6f02d6bbdc4b06a799fef
Dylan Portelance <dylanjportelance@gmail.com>
2012-03-23 20:03:51 -0700

    created notepath class

---
+++ GeometryTunes/NotePath.m
@@ -0,0 +1,13 @@
+//
+//  NotePath.m
+//  GeometryTunes
+//
+//  Created by Dylan Portelance on 3/23/12.
+//  Copyright (c) 2012 Tufts University. All rights reserved.
+//
+
+#import "NotePath.h"
+
+@implementation NotePath
+
+@end
