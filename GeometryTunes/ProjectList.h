b6d267ac6d809860be58c6872ea76c971b25b86f
trotha01 <Music@mlab7.music.tufts.edu>
2012-04-29 20:45:08 -0400

    new view for projects list

---
+++ GeometryTunes/ProjectList.h
@@ -0,0 +1,13 @@
+//
+//  ProjectList.h
+//  GeometryTunes
+//
+//  Created by Music2 on 4/29/12.
+//  Copyright (c) 2012 Tufts University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProjectList : UIViewController
+
+@end
